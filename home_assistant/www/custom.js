b5dc2e281800728b1d42cf927c95b3860ebb475f
parautenbach <parautenbach@gmail.com>
2020-12-22 21:15:48 +0200

    Custom colours
    But can't seem to use it in the mini graph card, since it can't take a variable for line_color.

--- home_assistant/www/custom.js
+++ home_assistant/www/custom.js
@@ -11,4 +11,13 @@ And put the following into <config-dir>/www/ThemeOverride.js
 https://community.home-assistant.io/t/rounded-corners-in-every-theme/103833/6?u=parautenbach
 */
 
-document.documentElement.style.setProperty('--ha-card-border-radius', '10px');
+/*
+Also see: https://github.com/home-assistant/frontend/blob/master/src/resources/ha-style.ts
+*/
+
+document.documentElement.style.setProperty('--ha-card-border-radius',       '10px');
+document.documentElement.style.setProperty('--custom-graph-color-blue',     '#3498db');  // slate: #2980b9 - 2b84d2
+document.documentElement.style.setProperty('--custom-graph-color-yellow',   '#ff9800');  // slate: #b58e31 - fd8509
+document.documentElement.style.setProperty('--custom-graph-color-red',      '#e74c3c');  // slate: #b83829 - df342e
+document.documentElement.style.setProperty('--custom-graph-color-green',    '#70a03c');  // slate: #70a03c - 107001
+document.documentElement.style.setProperty('--custom-graph-color-magenta',  '#8840a7');  // slate: #xxxxxx - 8840a7

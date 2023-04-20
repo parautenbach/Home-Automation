88540cb79b5d8a290a6a55371f271f8e7b71bd64
Pieter Rautenbach <parautenbach@gmail.com>
2023-04-20 21:58:43 +0200

    Recurse packages too

--- scripts/list_all_automations.sh
+++ scripts/list_all_automations.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-sed -nr 's/- alias.+"(.+)"/\1/p' * |sed -r "s/[ ']/_/g" |tr "[:upper:]" "[:lower:]" |sort |iconv -t ascii//translit |sed "s/'//g" |sed -r 's/^/- automation./g'
+find . -type f -name "*.yaml" -exec sed -nr 's/- alias.+"(.+)"/\1/p' {} + |sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' |sed -r "s/[ ']/_/g" |tr "[:upper:]" "[:lower:]" |sort |iconv -t ascii//translit |sed "s/'//g" |sed -r 's/^/- automation./g'
+

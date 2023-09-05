672811c40873c961600736dec9faf5b76414661b
Pieter Rautenbach <parautenbach@gmail.com>
2023-09-05 12:55:43 +0200

    Improved script
    No more manual commenting out.

--- scripts/list_all_automations.sh
+++ scripts/list_all_automations.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
+# run this from within the home_assistant directory of this repo
 
-find . -type f -name "*.yaml" -exec sed -nr 's/- alias.+"(.+)"/\1/p' {} + |sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' |sed -r "s/[ ']/_/g" |tr "[:upper:]" "[:lower:]" |sort |iconv -t ascii//translit |sed "s/'//g" |sed -r 's/^/- automation./g'
-
+find . -type f -name "*.yaml" -exec sed -nr 's/- alias.+"(.+)"/\1/p' {} + |sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' |sed -r "s/[ ']/_/g" |tr "[:upper:]" "[:lower:]" |sort |iconv -t ascii//translit |sed "s/'//g" |sed -r 's/^/- automation./g' |grep -v -f ../scripts/excluded_automations.txt

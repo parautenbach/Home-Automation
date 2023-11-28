aa233c8c5605b55b2871c9d3b1f3be4bafda465c
Pieter Rautenbach <parautenbach@gmail.com>
2023-11-28 22:17:23 +0200

    Damn memory leak won't go away
    Run the script like a cron.

--- scripts/micstatus.py
+++ scripts/micstatus.py
@@ -2,41 +2,36 @@
 # https://gist.github.com/ov1d1u/8e6ec1f0a087d1f410c65f16217273ba
 # because of this bug/deficiency:
 # https://github.com/home-assistant/iOS/issues/1903
+# plist commands:
+# https://davidhamann.de/2018/03/13/setting-up-a-launchagent-macos-cron/
 
 import time
 import requests
-import importlib
+import atomacos
 
-from multiprocessing import Pool
+CONTROL_CENTER = atomacos.getAppRefByBundleId('com.apple.controlcenter')
+HA_API_URL = "http://<host>:<port>/api/webhook/gaia_audio_input_in_use_alt"
 
-def run(mic_state_prev):
-    atomacos = importlib.import_module('atomacos')
+mic_status = False
 
-    control_center = atomacos.getAppRefByBundleId('com.apple.controlcenter')
-    ha_api_url = "http://<host>:<port>/api/webhook/gaia_audio_input_in_use_alt"
+with open('/tmp/micstatus.txt', 'r') as f:
+    mic_status_prev = f.read().strip().lower() == 'true'
 
-    # these two calls leak memory (40MB -> 500MB in a few hours)
-    elems = [elem for elem in control_center.findAllR() if 'AXDescription' in elem.getAttributes()]
-    descs = [elem for elem in elems if 'Microphone' in elem.AXDescription.split() and 'use' in elem.AXDescription.split()]
-    # True if list isn't empty
-    mic_state = bool(descs)
-    # print('On' if mic_state else 'Off')
+elems = [elem for elem in CONTROL_CENTER.findAllR() if 'AXDescription' in elem.getAttributes()]
+descs = [elem for elem in elems if 'Microphone' in elem.AXDescription.split() and 'use' in elem.AXDescription.split()]
 
-    if mic_state != mic_state_prev:
-        # print('State changed')
-        try:
-            response = requests.put(ha_api_url, data=[('status', 'on' if mic_state else 'off')])
-            # print(response.status_code)
-        except Exception as e:
-            # print(e)
-            pass
+# True if list isn't empty
+mic_status = bool(descs)
+# print('On' if mic_status else 'Off')
 
-    return mic_state
+if mic_status != mic_status_prev:
+    # print('State changed')
+    try:
+        requests.put(HA_API_URL, data=[('status', 'on' if mic_status else 'off')])
+        # print(response.status_code)
+    except Exception as e:
+        # print(e)
+        pass
 
-if __name__ == '__main__':
-    mic_state_prev = False
-
-    with Pool(processes=1) as pool:
-        while True:
-            mic_state_prev = pool.apply(run, args=(mic_state_prev,))
-            time.sleep(3)
+    with open('/tmp/micstatus.txt', 'w') as f:
+        f.write(str(mic_status).lower())
---
+++ scripts/micstatus_multiprocessing.py
@@ -0,0 +1,42 @@
+# source/credit:
+# https://gist.github.com/ov1d1u/8e6ec1f0a087d1f410c65f16217273ba
+# because of this bug/deficiency:
+# https://github.com/home-assistant/iOS/issues/1903
+
+import time
+import requests
+import importlib
+
+from multiprocessing import Pool
+
+def run(mic_state_prev):
+    atomacos = importlib.import_module('atomacos')
+
+    control_center = atomacos.getAppRefByBundleId('com.apple.controlcenter')
+    ha_api_url = "http://<host>:<port>/api/webhook/gaia_audio_input_in_use_alt"
+
+    # these two calls leak memory (40MB -> 500MB in a few hours)
+    elems = [elem for elem in control_center.findAllR() if 'AXDescription' in elem.getAttributes()]
+    descs = [elem for elem in elems if 'Microphone' in elem.AXDescription.split() and 'use' in elem.AXDescription.split()]
+    # True if list isn't empty
+    mic_state = bool(descs)
+    # print('On' if mic_state else 'Off')
+
+    if mic_state != mic_state_prev:
+        # print('State changed')
+        try:
+            response = requests.put(ha_api_url, data=[('status', 'on' if mic_state else 'off')])
+            # print(response.status_code)
+        except Exception as e:
+            # print(e)
+            pass
+
+    return mic_state
+
+if __name__ == '__main__':
+    mic_state_prev = False
+
+    with Pool(processes=1) as pool:
+        while True:
+            mic_state_prev = pool.apply(run, args=(mic_state_prev,))
+            time.sleep(3)

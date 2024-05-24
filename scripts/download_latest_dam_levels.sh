092badb5793b0e26f758c70f800a1b7f30b8f8df
Pieter Rautenbach <parautenbach@gmail.com>
2024-05-24 20:19:22 +0200

    Fixing the dam levels download (again)

--- scripts/download_latest_dam_levels.sh
+++ scripts/download_latest_dam_levels.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# https://www.capetown.gov.za/Family%20and%20home/residential-utility-services/residential-water-and-sanitation-services/this-weeks-dam-levels
-# https://web1.capetown.gov.za/web1/opendataportal/DatasetDetail?DatasetName=Dam%20levels
-# replaced by: https://odp-cctegis.opendata.arcgis.com/documents/dam-levels/about
-# 1. navigate to the above
-# 2. click the "open document" button above the summary and to the right of the big blue file icon
-# 3. on the next page, click on the file icon to download the latest file and check its contents
-# 4. now open the browser's inspector and search the dom for "downloadhandler"
-# 5. copy the link and test it in the browser (look out for a incorrectly encoded &amp; between .csv and DatasetDocument and replace it with &)
-# 6. replace the link below
-/usr/bin/curl -s --request GET --url "https://www.capetown.gov.za/_layouts/OpenDataPortalHandler/DownloadHandler.ashx?DocumentName=Dam+levels+2012+to+2024.csv&DatasetDocument=https%3A%2F%2Fcityapps.capetown.gov.za%2Fsites%2Fopendatacatalog%2FDocuments%2FDam%2520levels%2FDam%2520levels%25202012%2520to%25202024.csv" | tail -n 10 | awk '!/,,,/{line=$0} END{print line}' - > /home/homeassistant/.homeassistant/downloads/latest_dam_levels.csv
+# 1. go to https://odp-cctegis.opendata.arcgis.com/.
+# 2. go to "all data sets".
+# 3. search for "dam".
+# 4. pick "dam levels".
+# 5. it should take you here: https://odp-cctegis.opendata.arcgis.com/datasets/afddd5afd6f948edb35b9be669343fca/explore.
+# 6. in the bottom left corner, click "i want to use this".
+# 7. click on "open in arcgis online".
+# 8. a new tab should open and take you here: # https://www.arcgis.com/home/item.html?id=afddd5afd6f948edb35b9be669343fca
+# 9. click the image icon that says download.
+/usr/bin/curl -s --request GET --url "https://www.arcgis.com/sharing/rest/content/items/afddd5afd6f948edb35b9be669343fca/data" | tail -n 10 | awk '!/,,,/{line=$0} END{print line}' - > /home/homeassistant/.homeassistant/downloads/latest_dam_levels.csv

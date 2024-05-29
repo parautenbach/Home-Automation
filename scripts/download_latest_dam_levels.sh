964109337e2a1b03c626f83dbf572d7f0cc6b556
Pieter Rautenbach <parautenbach@gmail.com>
2024-05-29 21:04:29 +0200

    Ron Swanson: Dammit!

--- scripts/download_latest_dam_levels.sh
+++ scripts/download_latest_dam_levels.sh
@@ -9,4 +9,7 @@
 # 7. click on "open in arcgis online".
 # 8. a new tab should open and take you here: # https://www.arcgis.com/home/item.html?id=afddd5afd6f948edb35b9be669343fca
 # 9. click the image icon that says download.
-/usr/bin/curl -s --request GET --url "https://www.arcgis.com/sharing/rest/content/items/afddd5afd6f948edb35b9be669343fca/data" | tail -n 10 | awk '!/,,,/{line=$0} END{print line}' - > /home/homeassistant/.homeassistant/downloads/latest_dam_levels.csv
+# /usr/bin/wget https://www.arcgis.com/sharing/rest/content/items/fc2bbde9663d4a18a1b9fc3c6a38a0da/data -O /home/homeassistant/.homeassistant/downloads/latest_dam_levels.zip
+# /usr/bin/unzip -o -d /home/homeassistant/.homeassistant/downloads/ /home/homeassistant/.homeassistant/downloads/latest_dam_levels.zip
+# /usr/bin/cat "/home/homeassistant/.homeassistant/downloads/Dam levels 2012 to 2024.csv" | /usr/bin/tail -n 10 | /usr/bin/awk '!/,,,/{line=$0} END{print line}' - > /home/homeassistant/.homeassistant/downloads/latest_dam_levels.csv
+/usr/bin/curl -sL https://www.arcgis.com/sharing/rest/content/items/fc2bbde9663d4a18a1b9fc3c6a38a0da/data | /usr/bin/funzip | /usr/bin/tail -n 10 | /usr/bin/awk '!/,,,/{line=$0} END{print line}' > /home/homeassistant/.homeassistant/downloads/latest_dam_levels.csv

1c115c579cbf95cdc86ad24b5fab4d8cfa0e4d0c
Pieter Rautenbach <parautenbach@gmail.com>
2025-08-03 13:59:47 +0200

    Changes to the camera overlay
    White with a black border, now in the top right.

---
+++ scripts/record_security_camera_clip_with_overlay_and_audio.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# CONFIG
+STREAM_URL="rtsp://securitypi:8554/unicast"
+# HA will time out after 60 sec!
+DURATION="00:00:50"  # hh:mm:ss
+OUTPUT_DIR="/tmp"
+FONT_PATH="/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf"
+ENTITY_ID="camera.security_camera"
+
+# Generate timestamped filename: camera.security_camera_20250802-213118.mp4
+TIMESTAMP=$(date '+%Y%m%d-%H%M%S')
+OUTPUT_FILE="${OUTPUT_DIR}/${ENTITY_ID}_${TIMESTAMP}.mp4"
+
+# Run ffmpeg
+ffmpeg \
+  -rtsp_transport tcp -i "$STREAM_URL" \
+  -vf "drawtext=fontfile=${FONT_PATH}:text='%{localtime\:%Y-%m-%d %T}':x=w-text_w-10:y=10:fontsize=24:fontcolor=white@0.8:borderw=2:bordercolor=black@1:box=0" \
+  -c:v libx264 -preset ultrafast -crf 23 \
+  -c:a aac -b:a 128k \
+  -t "$DURATION" \
+  -y "$OUTPUT_FILE"
+  

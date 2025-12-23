8c75d57532748fae4f4917a6611e90983ea890be
Pieter Rautenbach <parautenbach@gmail.com>
2025-12-23 20:51:17 +0200

    Make pasting of an UUID more efficient

--- scripts/helpers/generate_uuid.sh
+++ scripts/helpers/generate_uuid.sh
@@ -2,4 +2,7 @@
 # alt: uuidgen | tr '[:upper:]' '[:lower:]'
 
 # use uuid4 to create random automation ids
-python -c 'import uuid; print(uuid.uuid4())'
+uuid=$(python -c 'import uuid; import sys; sys.stdout.write(str(uuid.uuid4()))')
+printf "%s" "$uuid" | pbcopy
+echo "$uuid"
+
